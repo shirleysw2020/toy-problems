6db8ed443ab038064ef6ae282bf497e8624a824e
Shirley Wu <shirleyswwu@gmail.com>
2020-07-08 19:56:43 -0700

    tried out the problem

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -18,8 +18,26 @@
 *
 */
 
-var rockPaperScissors = function (
-) {
+var rockPaperScissors = function (rounds) {
   // TODO: your solution here
+  var sequenceResult = [];
+  // calculate the maximum number of combinations we can have based on the input rounds.
+  // factorial won't work because RPS can have repeat letter: like RRS.
+  // total combination = rounds^rounds
+  var totalCombination = Math.pow(rounds, rounds);
+  // count for every letter is equal: totlaCombination/ 3
+
+  var r = totalCombination / 3;
+  var p = totalCombination / 3;
+  var s = totalCombination / 3;
+  // loop for number of input rounds
+    // loop for
+  // for (var i = 0; i <= r.length; i++)
+  var count = 0;
+
+  // concat letters onto R
+  // use Math.min(r, p, s) to choose which one to concat everytime into string
+
+
 };
 
