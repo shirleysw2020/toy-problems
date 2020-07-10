5a0a514cb4bcaa55dfcf21af4af37a38948ca3a1
Shirley Wu <shirleyswwu@gmail.com>
2020-07-09 19:48:24 -0700

    completed

--- evenOccurrence/evenOccurrence.js
+++ evenOccurrence/evenOccurrence.js
@@ -10,29 +10,54 @@
  * console.log(onlyEven); //  4
 */
 
+//1st approach:
+// var evenOccurrence = function(arr) {
+//   // Your code here.
+//   if (arr.length === 0) {return null;}
+//   //handle multiple even-occurrence items and return the first one - return the lesser i!
+//   var count = 1;
+//   // outer loop starts from first element
+//   for (var i = 0; i < arr.length; i++) {
+//     // inner loop starts from second element
+//     for (var j = i + 1; j < arr.length; j++) {
+//       // if j is equal to i
+//       if (arr[j] === arr[i]) {
+//         // count increment
+//         count++;
+//       }
+//     }
+//     // if number of times j occurs is even number
+//     if (count % 2 === 0) {
+//       // return value at i.
+//       return arr[i];
+//     }
+//   }
+// };
+
+//  8 5 8 8 9 my test doesn't work when rhere are 3 of the same number.
+
+//2nd method: slice out the numfrom the array when match with current nnumber:
 var evenOccurrence = function(arr) {
   // Your code here.
   if (arr.length === 0) {return null;}
-  if (arr[0] === arr[1]) {return arr[0];}
-  //handle multiple even-occurrence items and return the first one - return the lesser i!
-  var count = 1;
-  // outer loop starts from first element
+  var sliced = 0;
+
   for (var i = 0; i < arr.length; i++) {
-    // inner loop starts from second element
+    slice = 0;
     for (var j = i + 1; j < arr.length; j++) {
-      // if j is equal to i
       if (arr[j] === arr[i]) {
-        // count increment
-        count++;
+        // [2, 8, 2, 8, 8, 9]. example
+        // slice out the matched value
+        arr.slice(j, 1)
+        sliced++;
       }
-    }
-    // if number of times j occurs is even number
-    if (count % 2 === 0) {
-      // return value at i.
-      return arr[i];
+      if (sliced % 2 === 1) {return arr[i];}
+      // slice out i because we found no match for it in entire arr
+      arr.slice(i, 1)
     }
   }
-};
-
-var onlyEven = evenOccurrence([1, 7, 2, 8, 5, 8, 8, 9, 6, 4]);
+}
+var onlyEven = evenOccurrence([1, 7, 2, 4, 5, 6, 8, 9, 6, 4]);
 console.log(onlyEven); //  4
+
+//if find the same, slice out the element from array.
--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -39,5 +39,64 @@ var rockPaperScissors = function (rounds) {
   // use Math.min(r, p, s) to choose which one to concat everytime into string
 
 
+  // var choices = ['rock', 'paper', 'scissors'];
+  // var reuslt = [];
+  // //process current element
+
+  // // iterate over choices
+  // result.push(choice)
+  //   // recurse (do i need to save this?)
+  //   rockPaperScissors(rounds);
+  //   // undo the last thing you did to make this work
+  //   result.pop();
+  // // return results
 };
 
+
+var arrangingCoins = function(totalCoins) {
+  var currentCount = 0;
+  if (count === totalCoins) {
+    return
+  }
+
+  arrangingCoins(count + 1);
+}
+
+var arrangingCoins = function(totalCoins) {
+  var numRows = 0;
+  while (totalCoins > 1) {
+    totalCoins -= numRows;
+    numRows++;
+  }
+
+  return totalCoins < 0 ? numRows - 2 : numRows - 1;
+}
+
+var rockPaperScissors = function (rounds = 3) { // set 3 as default
+  if(rounds === 0) {return [];}
+
+  var options = ['R', 'P', 'S'];
+  var result = [];
+  // declare a helper function
+  var findAllCombinations = function(combination) {
+    // base case:
+    // if the length of combination is equal to the input rounds
+    if(combination.length === rounds) {
+      // push the combination to the result array
+      result.push(combination);
+      // return to for loop
+      return;
+    }
+    // recursion case:
+    // iterate the options array
+    for (var i = 0; i < options.length; i++) {
+      // add the current option of options array to the combination string
+      var currentCombination = combination + options[i]
+      // call helper function with the updated combination string
+      findAllCombinations(currentCombination);
+    }
+  }
+  // call helper function
+  findAllCombinations('');
+  return result;
+};
