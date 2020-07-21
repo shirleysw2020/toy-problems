340aae1a22f8e2a5872ba4bd491292d5d8990c74
Shirley Wu <shirleyswwu@gmail.com>
2020-07-20 19:03:16 -0700

    completed propblem

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -8,4 +8,21 @@
 
 var firstNonRepeatedCharacter = function(string) {
   // TODO: your solution here
+  // have an object to keep track of each character and the num of times they appeared
+  // seach for the key that has only one occurence as its key value and return that key
+
+  const storage = {};
+
+  for (var i = 0; i < string.length; i++) {
+    storage[string[i]] = (storage[string[i]] + 1) || 1;
+  }
+
+  for (var key in storage) {
+    if (storage[key] === 1) {
+      return key;
+    }
+  }
 };
+
+console.log(firstNonRepeatedCharacter('ABA')); // => 'B'
+console.log(firstNonRepeatedCharacter('AACBDB')); // => 'C'
