acb67c5fce3f24c9e2f0d228ca46ea2dcc15f19c
Shirley Wu <shirleyswwu@gmail.com>
2020-07-21 20:00:14 -0700

    getting one repeat string

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -14,18 +14,36 @@
 
 var allAnagrams = function(string) {
   // Your code here.
-
   if(string === 0) {return [];}
   if (string.length === 1) {return [string]; }
-  var result = [];
+  var result = [string];
   // push initial/input string into result
-  // alternate characters (string.length - 1 times)...meaning: keep pushing 1st char as last char)
+  // alternate characters (string.length - 1 times) --> meaning: keep pushing 1st char to last char)
       // push new string into result
   // at the end of the last alternation (we reached string - 1 times)
     // swap first two characters
     // repeat line 17/18
-
-
+  var times = 0;
+  while (times < string.length - 1) {
+    for (var i = 0; i < string.length - 1; i++) {
+      var restOfStr = string.slice(1);
+      // slice out first char
+      var first = string.slice(0,1);
+      // append it to the end of string
+      var alterStr = restOfStr.concat(first);
+      string = alterStr
+      result.push(alterStr);
+      // save first and second chat for swapping
+      var oldFirst = string[0];
+      var oldSecond = string[1]
+      var keepRest = string.slice(2);
+    }
+    // swap first and second characters
+    string = oldSecond.concat(oldFirst).concat(keepRest);
+    result.push(string);
+    times++;
+  }
+  return result;
 
 };
 
