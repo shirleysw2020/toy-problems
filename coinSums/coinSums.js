4e7f7c5ce0ca73fae0c444b90cbb6b5aa40762be
Shirley Wu <shirleyswwu@gmail.com>
2020-07-23 20:03:23 -0700

    completed

--- coinSums/coinSums.js
+++ coinSums/coinSums.js
@@ -25,7 +25,30 @@ makeChange(2) === 2
 */
 
 var makeChange = function(total) {
+  var count = 0;
+  var coins = [1, 2, 5, 10, 20, 50, 100, 200];
 
+  var changer = function(index, value) {
+    if (index < 0) {
+      return;
+    }
+    var currentCoin = coins[index];
+
+    // if (index === 0) {
+      if (value % currentCoin === 0) {
+        count++;
+        // return;
+      }
+
+    // }
+
+    while (value >= 0) {
+      changer(index-1, value);
+      value -= currentCoin;
+    }
+  }
+  changer(coins.length-1, total);
+  return count;
 };
 
-
+console.log(makeChange(200));
