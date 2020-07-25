5b7b925f600f2d345c46391b88e3440387514d30
Shirley Wu <shirleyswwu@gmail.com>
2020-07-24 19:56:27 -0700

    pushing partial results into solution

--- powerSet/powerSet.js
+++ powerSet/powerSet.js
@@ -7,15 +7,34 @@
  * powerSet("abc")
  * -> [ '' , 'a', 'b', 'c', 'ab', 'ac', 'bc', 'abc' ]
  *
- * Note: 
+ * Note:
  *  1. All characters in a subset should be sorted.
- *  2. Sets of the same characters are considered duplicates regardless of order and count only once, e.g. 'ab' and 'ba' are the same. 
- * 
+ *  2. Sets of the same characters are considered duplicates regardless of order and count only once, e.g. 'ab' and 'ba' are the same.
+ *
  * Example 2 :
- * 
+ *
  * powerSet("jump")
  * -> ["", "j", "ju", "jm", "jp", "jmu", "jmp", "jpu", "jmpu", "u", "m", "p", "mu", "mp", "pu", "mpu"]
  */
 
 var powerSet = function(str) {
+  /* (0,1         1,2
+      0,2         1,3
+      0,3         1,4
+      0,4)        1,5
+  */
+  var res = [''];
+  var i = 0
+  var y = i + 1;
+  // var incrementation = 1;
+    while (i < str.length) {
+      for (var y = i + 1;  y <= str.length; y++) {
+        res.push(str.slice(i, y))
+      }
+      i++;
+    }
+  return res;
 };
+
+
+console.log(powerSet('jump'));
