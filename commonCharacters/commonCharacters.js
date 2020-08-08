cf128e5a60b5990b6c3ddce21e64b2fa23214315
Shirley Wu <shirleyswwu@gmail.com>
2020-08-07 19:12:55 -0700

    completed

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -14,4 +14,22 @@
 
 var commonCharacters = function(string1, string2) {
   // TODO: Your code here!
+  var strObj1 = {}, strObj2 = {}, resStr = '';
+  // loop through first string and put all characters in strObj1
+  for (var c of string1) {
+    strObj1[c] = (strObj1[c] + 1) || 1;
+  }
+
+  for (var c of string2) {
+    strObj2[c] = (strObj2[c] + 1) || 1;
+  }
+  // loop over strObj1, check if same key exist in strObj2
+  for (var char in strObj1) {
+    if (strObj1[char] === strObj2[char]) {
+      // add character to result str
+      resStr += char;
+    }
+  }
+  return resStr;
 };
+
