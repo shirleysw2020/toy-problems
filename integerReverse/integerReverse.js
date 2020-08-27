36dc861d98a8e9ac3fc710f9dad6db3bf174af6c
Shirley Wu <shirleyswwu@gmail.com>
2020-08-26 19:59:45 -0700

    completed

--- integerReverse/integerReverse.js
+++ integerReverse/integerReverse.js
@@ -1,14 +1,24 @@
-/* 
-* 
+/*
+*
 * Integer Reverse
-* 
-* Given a positive integer, return its digits reversed. 
-* 
-* - DO NOT CONVERT THE INPUT INTO A STRING OR ARRAY. 
+*
+* Given a positive integer, return its digits reversed.
+*
+* - DO NOT CONVERT THE INPUT INTO A STRING OR ARRAY.
 * - Only use integers and math in your solution.
 *
 */
 
-function reverseInteger(number){
-  // TODO: Implement this function!
+//876
+function reverseInteger(number) {
+  var revNumber = 0;
+  while (number > 0) {
+    revNumber = (revNumber * 10) + (number % 10);
+    number = Math.floor(number / 10);
+  }
+  return revNumber;
 }
+// console.log('mode 10:', 876 % 10)
+// console.log('678 mode 10', 678 % 10)
+// console.log('876 / 10 is: ', 876 / 10)
+console.log(reverseInteger(876));
